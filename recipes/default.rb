7b6f038ce7ff6b7b1d6c31696636053d80d6efe3
Eric Strong <x00169645@mytudublin.ie>
2020-02-24 22:26:53 +0000

    configuration management

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,8 +5,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
-execute "apt-get update" do
-    command "apt-get update"
+execute "yum update" do
+    command "yum update"
 end
 
 package "apache2" do
@@ -20,4 +20,19 @@ end
 cookbook_file "/var/www/html/index.html" do
     source "index.html"
     mode "0644"
+end
+
+directory '/home/ec2-user' do
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+end
+
+
+file '/home/ec2-user/configurationmanagement' do
+    content '<html>This is a placeholder for the home page.</html>'
+    mode '0755'
+    owner 'web_admin'
+    group 'web_admin'
 end
